4a6147e51304ae07096522ba7a8400616526a8af
marovichn <nikolamarovic.11@gmail.com>
2023-08-31 18:03:29 +0200

    Layout - 1

---
+++ synthify/app/(root)/page.tsx
@@ -0,0 +1,4 @@
+const Home = () => {
+  return <div className='p-5 text-6xl text-green-500'>Main Content</div>;
+};
+export default Home;
---
+++ synthify/components/Library.tsx
@@ -0,0 +1,25 @@
+"use client";
+
+import { FC } from "react";
+import { TbPlaylist } from "react-icons/tb";
+
+interface LibraryProps {}
+
+const Library: FC<LibraryProps> = ({}) => {
+  const onClick = () => {
+    //handle upload later
+  };
+
+  return (
+    <div className='flex flex-col'>
+      <div className='flex items-center justify-between px-5 pt-4'>
+        <div className='inline-flex items-center gap-x-2'>
+          <TbPlaylist size={26} className='text-neutral-400' />
+          <p className='text-neutral-400 font-bold text-md'>Your Library</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Library;
